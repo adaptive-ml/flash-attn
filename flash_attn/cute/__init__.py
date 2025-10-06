76b678de77d83fa09b864b21dc627fb6e5cf82f5
imbr92 <40306754+imbr92@users.noreply.github.com>
2025-10-06 17:04:50 +0000

    Bumping cute-dsl version back and swapping masking (old fixed len workarounds)

--- flash_attn/cute/__init__.py
+++ flash_attn/cute/__init__.py
@@ -3,11 +3,26 @@
 from .interface import (
     flash_attn_func,
     flash_attn_varlen_func,
+    _flash_attn_fwd,
+    _flash_attn_bwd,
 )
+
+from .flash_bwd_preprocess import (
+    FlashAttentionBackwardPreprocess,
+)
+
+from .flash_bwd import (
+    FlashAttentionBackwardSm80,
+)
+
 
 __version__ = "0.1.0"
 
 __all__ = [
     "flash_attn_func",
     "flash_attn_varlen_func",
+    "_flash_attn_fwd",
+    "_flash_attn_bwd",
+    "FlashAttentionBackwardPreprocess"
+    "FlashAttentionBackwardSm80",
 ]
